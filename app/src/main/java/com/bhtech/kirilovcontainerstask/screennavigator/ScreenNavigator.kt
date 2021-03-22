ae1a773f2f4788ea7dd1057cb69a84d1fc0780a4
Veradux <veradux@abv.bg>
2021-03-23 01:33:37 +0200

    Separate screen navigator impl into two functions, for clarity

--- app/src/main/java/com/bhtech/kirilovcontainerstask/screennavigator/ScreenNavigator.kt
+++ app/src/main/java/com/bhtech/kirilovcontainerstask/screennavigator/ScreenNavigator.kt
@@ -1,15 +1,15 @@
 package com.bhtech.kirilovcontainerstask.screennavigator
 
-/**
- * Contains all available screen fragments.
- *
- * Used by ScreenNavigator as a way to tell which fragment to place in the main activity.
- */
-enum class Screen {
-    LOGIN
-}
-
 interface ScreenNavigator {
 
     fun navigateTo(screen: Screen)
+
+    /**
+     * Contains all available screen fragments.
+     *
+     * Used by ScreenNavigator as a way to tell which fragment to place in the main activity.
+     */
+    enum class Screen {
+        LOGIN
+    }
 }
--- app/src/main/java/com/bhtech/kirilovcontainerstask/screennavigator/ScreenNavigatorImpl.kt
+++ app/src/main/java/com/bhtech/kirilovcontainerstask/screennavigator/ScreenNavigatorImpl.kt
@@ -2,19 +2,22 @@ package com.bhtech.kirilovcontainerstask.screennavigator
 
 import androidx.fragment.app.FragmentActivity
 import com.bhtech.kirilovcontainerstask.R
+import com.bhtech.kirilovcontainerstask.screennavigator.ScreenNavigator.Screen
 import com.bhtech.kirilovcontainerstask.ui.login.LoginFragment
 import javax.inject.Inject
 
 class ScreenNavigatorImpl @Inject constructor(private val activity: FragmentActivity) : ScreenNavigator {
 
     override fun navigateTo(screen: Screen) {
-        val requestedFragment = when (screen) {
-            Screen.LOGIN -> LoginFragment()
-        }
+        val requestedFragment = getCorrespondingFragmentTo(screen)
 
         activity.supportFragmentManager.beginTransaction()
             .replace(R.id.container, requestedFragment)
             .addToBackStack(requestedFragment::class.java.canonicalName)
             .commit()
+    }
+
+    private fun getCorrespondingFragmentTo(screen: Screen) = when (screen) {
+        Screen.LOGIN -> LoginFragment()
     }
 }
