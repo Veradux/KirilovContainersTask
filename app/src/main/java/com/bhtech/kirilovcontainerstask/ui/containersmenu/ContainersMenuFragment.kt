b584b62f93652c976ef328402971122a5f5f2d34
Veradux <veradux@abv.bg>
2021-03-24 18:59:36 +0200

    Remove unused MarginItemDecoration.kt

--- app/src/main/java/com/bhtech/kirilovcontainerstask/ui/containersmenu/ContainersMenuFragment.kt
+++ app/src/main/java/com/bhtech/kirilovcontainerstask/ui/containersmenu/ContainersMenuFragment.kt
@@ -10,7 +10,6 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import com.bhtech.kirilovcontainerstask.R
 import com.bhtech.kirilovcontainerstask.databinding.FragmentContainersMenuBinding
-import com.bhtech.kirilovcontainerstask.ui.util.MarginItemDecoration
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -25,9 +24,7 @@ class ContainersMenuFragment : Fragment() {
     }
 
     private fun setUpContainersRecyclerView(recyclerView: RecyclerView) {
-        val margin = resources.getDimension(R.dimen.screen_edge_margin).toInt()
         recyclerView.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
-
         recyclerView.adapter = ContainersAdapter().also { it.submitList(viewModel.getContainers()) }
     }
 }
