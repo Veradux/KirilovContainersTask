e99cdd125cd8584e4fce63ede8bf5eedafd680dd
Veradux <veradux@abv.bg>
2021-03-24 20:59:15 +0200

    Move service call to a different thread using coroutines, and add a progress bar until the call is complete

--- app/src/main/java/com/bhtech/kirilovcontainerstask/ui/containersmenu/ContainersMenuFragment.kt
+++ app/src/main/java/com/bhtech/kirilovcontainerstask/ui/containersmenu/ContainersMenuFragment.kt
@@ -6,25 +6,56 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
-import com.bhtech.kirilovcontainerstask.R
 import com.bhtech.kirilovcontainerstask.databinding.FragmentContainersMenuBinding
+import com.bhtech.kirilovcontainerstask.service.containers.model.Container
+import com.bhtech.kirilovcontainerstask.ui.containersmenu.ContainersMenuViewModel.ContainersState
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ContainersMenuFragment : Fragment() {
 
     private val viewModel: ContainersMenuViewModel by viewModels()
+    private lateinit var binding: FragmentContainersMenuBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val binding = FragmentContainersMenuBinding.inflate(inflater, container, false)
+        binding = FragmentContainersMenuBinding.inflate(inflater, container, false)
         setUpContainersRecyclerView(binding.rvContainers)
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewModel.containersState.observe(viewLifecycleOwner, Observer { onContainersStateChange(it) })
+        viewModel.loadContainers()
+    }
+
     private fun setUpContainersRecyclerView(recyclerView: RecyclerView) {
         recyclerView.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
-        recyclerView.adapter = ContainersAdapter().also { it.submitList(viewModel.getContainers()) }
+    }
+
+    private fun onContainersStateChange(containersState: ContainersState) {
+        when (containersState) {
+            is ContainersState.Loading -> showContainersLoading()
+            is ContainersState.Empty -> showEmptyState()
+            is ContainersState.Loaded -> showContainers(containersState.containers)
+        }
+    }
+
+    private fun showContainers(containers: List<Container>) {
+        binding.pbContainersLoadingSpinner.visibility = View.GONE
+        val adapter = ContainersAdapter()
+        binding.rvContainers.adapter = adapter
+        adapter.submitList(containers)
+    }
+
+    private fun showContainersLoading() {
+        binding.pbContainersLoadingSpinner.visibility = View.VISIBLE
+    }
+
+    private fun showEmptyState() {
+        binding.pbContainersLoadingSpinner.visibility = View.GONE
+        binding.tvNoContainersLoadedMessage.visibility = View.VISIBLE
     }
 }
--- app/src/main/java/com/bhtech/kirilovcontainerstask/ui/containersmenu/ContainersMenuViewModel.kt
+++ app/src/main/java/com/bhtech/kirilovcontainerstask/ui/containersmenu/ContainersMenuViewModel.kt
@@ -1,9 +1,14 @@
 package com.bhtech.kirilovcontainerstask.ui.containersmenu
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.bhtech.kirilovcontainerstask.service.containers.ContainersService
 import com.bhtech.kirilovcontainerstask.service.containers.model.Container
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,6 +16,23 @@ class ContainersMenuViewModel @Inject constructor() : ViewModel() {
 
     @Inject lateinit var containersService: ContainersService
 
-    // TODO don't do this on the UI thread
-    fun getContainers(): List<Container> = containersService.getAllContainers()
+    sealed class ContainersState {
+        object Loading : ContainersState()
+        object Empty : ContainersState()
+        data class Loaded(val containers: List<Container>) : ContainersState()
+    }
+
+    val containersState = MutableLiveData<ContainersState>().apply { value = ContainersState.Loading }
+
+    fun loadContainers() {
+        val backgroundJob = viewModelScope.async(Dispatchers.IO) { containersService.getAllContainers() }
+        viewModelScope.launch { receiveContainers(backgroundJob.await()) }
+    }
+
+    private fun receiveContainers(list: List<Container>) {
+        when {
+            list.isEmpty() -> containersState.value = ContainersState.Empty
+            list.isNotEmpty() -> containersState.value = ContainersState.Loaded(list)
+        }
+    }
 }
